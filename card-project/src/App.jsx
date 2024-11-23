9011d43bb24a6dff7c6de6599396bd3d960aaade
AnkushSatpute2005 <ankushsatpute2005@gmail.com>
2024-11-23 23:06:11 +0530

    Added Successfully

--- card-project/src/App.jsx
+++ card-project/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         return (
           <div key={d.id} className="w-1/5 m-6 p-3 rounded-lg shadow-2xl gap-3 justify-between bg-[#f5f0f7]">
             <b className="bg-[#ebd2fa] p-1 rounded  ">ID:{d.id}</b>
-            <h4><b>userId</b>:{d.userId}</h4>
+            <h4 className="p-4"><b>userId</b>:{d.userId}</h4>
             <p><b>title :</b> {d.title}</p>
             <p><b>body:</b>{d.body}</p>
           </div>
